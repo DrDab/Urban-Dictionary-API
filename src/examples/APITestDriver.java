9159f4645da4b3b11849343da2521482676aceb0
_c0da_ <duvictor514@gmail.com>
2018-06-07 22:13:00 -0700

    Added more examples to sample driver class

--- src/examples/APITestDriver.java
+++ src/examples/APITestDriver.java
@@ -1,5 +1,7 @@
 package examples;
 
+import org.json.JSONArray;
+
 import urbanapi.Definition;
 import urbanapi.UDParser;
 
@@ -8,13 +10,19 @@ public class APITestDriver
 	public static void main (String[] args)
 	{
 		UDParser udparser = new UDParser("http://api.urbandictionary.com/v0/define?term=");
-		Definition[] test = udparser.getDefinitionsByKeyword(udparser.getJSONData("windows+xp"));
+		String JSONData = udparser.getJSONData("minecraft");
+		Definition[] test = udparser.getDefinitionsByKeyword(JSONData);
 		for(int i = 0; i < test.length; i++)
 		{
 			System.out.println(test[i].getWordName());
 			System.out.println(test[i].getDefinition());
 			System.out.println(test[i].getWrittenDate());
 			System.out.println(test[i].getPermalink());
+		}
+		JSONArray keywordTags = udparser.getTagsByKeyword(JSONData);
+		for(int i = 0; i < keywordTags.length(); i++)
+		{
+			System.out.println(keywordTags.get(i));
 		}
 	}
 }
