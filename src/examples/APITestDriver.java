951e46d1b1c2c4d490474b932a71af90cb0e9d43
_c0da_ <duvictor514@gmail.com>
2018-06-07 22:08:44 -0700

    Updated driver example

--- src/examples/APITestDriver.java
+++ src/examples/APITestDriver.java
@@ -8,10 +8,12 @@ public class APITestDriver
 	public static void main (String[] args)
 	{
 		UDParser udparser = new UDParser("http://api.urbandictionary.com/v0/define?term=");
-		Definition[] test = udparser.getDefinitionsByKeyword(udparser.getJSONData("sadfjoasfjoj"));
+		Definition[] test = udparser.getDefinitionsByKeyword(udparser.getJSONData("windows+xp"));
 		for(int i = 0; i < test.length; i++)
 		{
-			System.out.println(test[i]);
+			System.out.println(test[i].getWordName());
+			System.out.println(test[i].getDefinition());
+			System.out.println(test[i].getWrittenDate());
 			System.out.println(test[i].getPermalink());
 		}
 	}
